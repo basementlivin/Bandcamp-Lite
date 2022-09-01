574df05c8d78ab29c3038f33cde6fabf86712552
badMeme <noveladdress@gmail.com>
2022-09-01 01:55:46 -0500

    refined db schemas and sample model population. Testing through CRUD

--- models/models.js
+++ models/models.js
@@ -6,14 +6,13 @@ const artistSchema = new Schema ({
     name: {type: String, required: true},
     members: [String,],
     bio: {String},
-    yearFormed: {Number}, //** new Date?
-    location: {String}, //*
+    yearFormed: {Number}, 
+    location: {String},
     active: {type: Boolean, default: true},
-    //discography: { 
-    // [{type: mongoose.Schema.Types.ObjectId()}]
-    // albums: [{String}], //*
-    // songs: [{String}], //*
-    // }
+    discography: { 
+        albums: [{type: mongoose.Types.ObjectId, ref: 'Album'},],
+        songs: [{type: mongoose.Types.ObjectId, ref: 'Song'},], 
+    },
     // genre: [{type: mongoose.Schema.Types.ObjectID()***}], //*
     media: [{String}], // array of URLs to assetts, album art, merch photos, etc, that the artist wants to display on page
     profilePicture: {String}, //URL to cover photo asset, can ref media array probably
@@ -23,18 +22,19 @@ const artistSchema = new Schema ({
 
 const albumSchema = new Schema ({
     title: {type: String, required: true},
-    artist: {type: mongoose.Schema.Types.ObjectId, ref: 'Artist'}, // * default to artist of page
-    tracks: [{String}], //make this ref song schema
-    albumart: {type: String}, //URL to cover art asset
+    artist: {type: mongoose.Types.ObjectId, ref: 'Artist'},
+    tracks: [{type: mongoose.Types.ObjectId, ref: 'Song'}], //make this ref song schema
+    albumart: String, //URL to cover art asset
     year: {Number},
-    genre: [{String}] //make this ref song schema
+    genre: [String] //make this ref song schema
 })
 
 const songSchema = new Schema ({
     title: {type: String, required: true},
     audio: {type: String}, //URL to audio assett in database 
     artist: {type: mongoose.Types.ObjectId, ref: 'Artist'}, //* default to artist of page
-    album: [{type: String, default: null}] //*  may need different default
+    album: {type: String, default: null}, //*  may need different default
+    genre: [String]
     //length? //*check out timestamp formatting
 })
 
--- models/seed_data.js
+++ models/seed_data.js
@@ -4,7 +4,7 @@ const { default: test } = require('node:test');
 ////
 ////temp server connection below
 ////
-const connectionStr = 'mongodb+srv://badmeme:RTSmKHjCJn1PyVGI@cluster0.engpp09.mongodb.net/test'
+const connectionStr = 'mongodb+srv://badmeme:RTSmKHjCJn1PyVGI@cluster0.engpp09.mongodb.net/project2'
 
 mongoose.connect(connectionStr);
 mongoose.connection.on('connected', () => {
@@ -33,113 +33,111 @@ const testArtist = ({
     yearFormed: 1985, //** new Date?
     location: "Abingdon, Oxfordshire", //*
     active: true,
-    // discography: {
-    //     albums: [], //*
-    //     songs: [] //*
-    // }
+    discography: {
+        albums: [], //*
+        songs: [] //*
+    },
     genre: ["Rock", "Electronic", "Experimental"], //* push genre from all songs ?
     profilePicture: "https://cdn.mos.cms.futurecdn.net/QyuRpsE7sBno5atYkzHK43-1920-80.jpg.webp", //URL to cover photo asset
 })
 
-const testAlbum = ({
+const testAlbum = ([
+    {
     title: "OK Computer",
-    artist: testArtist.name, // * default to artist of page
+    artist: "", // * default to artist of page
     tracks: [], // *
     albumart: "https://upload.wikimedia.org/wikipedia/en/b/ba/Radioheadokcomputer.png", //URL to cover art asset
     year: 1997,
     genre: ["Rock", "Experimental"]
-})
+    },
+])
 
 const testSongs = ([
     {
         title: "Airbag",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"] //*  
+        album: "OK Computer" //*  
         //length? //*check out timestamp formatting
     },
     {
         title: "Paranoid Android",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"] //* 
+        album: "OK Computer" //* 
         //length? //*check out timestamp formatting
     },
     {
         title: "Subterrenean Homesick Alien",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Exit Music (For a Film)",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Let Down",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Karma Police",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Fitter Happier",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Electioneering",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Climbinb Up the Walls",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "No Surprises",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "Lucky",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     },
     {
         title: "The Tourist",
         audio: "", //URL to audio assett in database 
         artist: newArtist._id, //* default to artist of page
-        album: ["OK Computer"]
+        album: "OK Computer"
     }
 ])
 
 ///---Creating test data---///
 console.log("creating test data");
 
-// async function populateSeedData() {
-    
-// }
-
 async function populateArtist () {
     try {
         let deleted = await Artist.deleteMany({});
         //console.log(deleted)
         //let reloading = await db.Artist.insertMany();
-        let reloading = await Artist.create({
+        let accountArtist = await Artist.create({
             name: testArtist.name,
             members: testArtist.members,
             yearFormed: testArtist.yearFormed,
@@ -149,7 +147,8 @@ async function populateArtist () {
             genre: testArtist.genre,
             profilePicture: testArtist.profilePicture
         });
-        //console.log(reloading)
+        console.log(accountArtist)
+        return accountArtist;
     }
     catch (err) {
         console.log(err)
@@ -157,31 +156,35 @@ async function populateArtist () {
     console.log("created test artist");
 }
 
-async function populateSongs () {
+async function populateAlbums( accountArtist ) {
+    try{
+        await Album.deleteMany();
+        let accountAlbum = await Album.create({
+            title: testAlbum[0].title,
+            artist: accountArtist._id,
+            tracks: [],
+            albumart: testAlbum[0].albumart,
+            year: testAlbum[0].year,
+            genre: []
+        })
+        console.log("Album:", accountAlbum)
+        return accountAlbum
+    }
+
+    catch (err) {
+        console.log(err)
+    }
+}
+
+async function populateSongs ( accountArtist, accountAlbum ) {
     try {
         let deleted = await Song.deleteMany({});
-        //console.log(deleted)
-        //let reloading = await db.Artist.insertMany();
-        //let tryingSomething = Artist.find({name: testArtist.name})
-        //console.log("trying something", tryingSomething)
-        Artist.find({name: testArtist.name}, async function (err, allArtists) {
-            for (i = 0; i < testSongs.length; i++) {
-                testSongs[i].artist = allArtists[0]._id
-                //console.log("allArtists", allArtists)
-                console.log (testSongs[i])    
-            }
-
-
-            //console.log("newArtist", newArtist)
-            await Song.insertMany(testSongs);
-        }); 
-        
-        //Album.find({name: })
-       // console.log(reloading)
-        //console.log("test", newArtist)
-        //testSongs
-        //console.log(newArtist._id)
-        
+        for (i = 0; i < testSongs.length; i++) {
+            testSongs[i].artist = accountArtist._id
+            //testSongs[i].album = accountAlbum.title
+        }
+        let accountSongs = await Song.insertMany(testSongs); 
+        return accountSongs       
     }
 
     catch (err) {
@@ -190,30 +193,47 @@ async function populateSongs () {
     console.log("created test songs");
 }
 
-async function populateAlbums() {
-    
+async function updateArtist ( inputA, inputB, inputC ) {
     try {
-        await Album.deleteMany({});
-        Artist.find({name: testArtist.name}, async function (err, allArtists) {
-            for (i = 0; i < testSongs.length; i++) {
-                for (j = 0; < )
-                testSongs[i].album = allArtists[0]._id
-                //console.log("allArtists", allArtists)
-                console.log (testSongs[i])    
-            }
+        let tempDiscog = inputA.discography
+        console.log("temp test", inputB._id)
+        tempDiscog.albums.push(inputB._id)
+        for (i =0 ; i < inputC.length; i++) {
+            tempDiscog.songs.push(inputC[i]._id)
+        }
+        console.log("final temp discog", tempDiscog)
+        await Artist.findByIdAndUpdate(inputA._id, {discography: tempDiscog})
+    }
+    
+    catch (err) {
+        console.log(err)
+    }
+}
 
-
-            //console.log("newArtist", newArtist)
-            await Song.insertMany(testSongs);
-        }); 
-    } 
+async function updateAlbum ( b, c ) {
+    try {
+        let tempTracks = b.tracks
+        //let tempGenre = b.genre
+        for (i = 0 ; i < c.length; i++) {
+            tempTracks.push(c[i]._id)
+            //tempGenre.push(c[i].genre[j] if its not in there)
+        }
+        await Album.findByIdAndUpdate(b._id, {tracks: tempTracks})
+    }
 
     catch (err) {
         console.log(err)
     }
 }
 
-//populateSeedData()
+async function populateSeedData() {
+    let artist = await populateArtist();
+    let album = await populateAlbums(artist);
+    let songs = await populateSongs(artist, album);
+    await updateArtist (artist, album, songs)
+    await updateAlbum (album, songs)
 
-//populateArtist();
-populateSongs();
+    mongoose.connection.close();
+}
+
+populateSeedData();
