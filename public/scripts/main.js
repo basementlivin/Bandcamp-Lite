6e5ac0da7d027bd320651663ea30dc945098384c
basementlivin <epedersen13@gmail.com>
2022-09-02 19:56:04 -0400

    Add event listener to "+ add another track"

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,9 @@
-console.log("John Prine rules.");
+console.log(`John Prine rules.`);
+
+const addAnotherTrack = document.getElementById("another-new-track");
+const newTrackField = document.createElement("div");
+newTrackField.setAttribute("class", "another-track-form-field");
+
+addAnotherTrack.addEventListener('click', (e) => {
+    console.log(`EVENT LISTENING!`)
+})
