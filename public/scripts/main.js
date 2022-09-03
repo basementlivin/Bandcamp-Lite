b174f82fb0ad1ec93da775cab4b249f70d721868
basementlivin <epedersen13@gmail.com>
2022-09-03 18:59:25 -0400

    CSS updates; refactor the click to add new track function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,6 +7,22 @@ function addAnotherTrackField() {
     console.log(`U DONE GONE & CLICKED IT`)
     let nextTrack = document.getElementById("another-track-form-field");
     let newTrackField = document.createElement('div');
-    newTrackField.innerHTML = "<div id='another-track-form-field'><label for='tracktitle'>Track Name</label><input type='text' placeholder='Ride Or Die' name='tracktitle' id='tracktitle'/></div>";
+    newTrackField.innerHTML =
+        `<div class="upload-form-field" id="another-track-form-field">
+            <label for="tracktitle">Track Name</label>
+                <input
+                type="text"
+                placeholder="Ride Or Die"
+                name="tracktitle"
+                id="tracktitle" 
+                />
+            <label for="audiofile">Audio File</label>
+                <input
+                type="file"
+                accept="audio/*"
+                name="audiofile"
+                id="audiofile"
+                />
+        </div>`
     nextTrack.appendChild(newTrackField);   
 }
